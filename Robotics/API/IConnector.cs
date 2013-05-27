424fbc2b19d433930c9fbf3d086635a852972d58
Mauricio Matamoros <mdk_bzk@hotmail.com>
2013-05-27 12:28:22 -0500

    Unstable commit point still under development. It is made in order to return to the master branch to change the Robotics.HAL.Sensors.Telemetric.LaserReading class for a quick sine/cosine calculation.

---
+++ Robotics/API/IConnector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robotics.API
+{
+	/// <summary>
+	/// Represents an object which allows asynchronous communication with other objects via message passing
+	/// </summary>
+	public interface IConnector : IService
+	{
+		#region Properties
+
+		#endregion
+
+		#region Events
+
+		/// <summary>
+		/// Occurs when a command is received
+		/// </summary>
+		event CommandReceivedEventHandler<IConnector> CommandReceived;
+
+		/// <summary>
+		/// Occurs when a response is received
+		/// </summary>
+		event ResponseReceivedEventHandler<IConnector> ResponseReceived;
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Sends a Command
+		/// </summary>
+		/// <param name="command">Command to be sent</param>
+		/// <returns>true if command was sent successfully, false otherwise</returns>
+		bool Send(Command command);
+
+		/// <summary>
+		/// Sends a Response
+		/// </summary>
+		/// <param name="response">Response to be sent</param>
+		/// <returns>true if response was sent successfully, false otherwise</returns>
+		bool Send(Response response);
+
+		#endregion
+	}
+}
