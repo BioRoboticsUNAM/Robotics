32c032d4916b79b9f91118c60cfa934816ec6698
Mauricio Matamoros <kyordhel@gmail.com>
2015-06-08 09:57:50 +0200

    Plugin Support
    Plugin functionality recovered from previous commit
    Compliant with windows only.

---
+++ Robotics/Plugins/IPlugin.cs
@@ -0,0 +1,31 @@
+﻿// #define DisposablePlugins
+
+using System;
+
+namespace Robotics.Plugins
+{
+
+	/// <summary>
+	/// Represents a Plugin
+	/// </summary>
+	public interface IPlugin : Robotics.IService
+#if DisposablePlugins
+		: IDisposable
+#endif
+	{
+		/// <summary>
+		/// Gets the name of the Plugin
+		/// </summary>
+		string Name { get; }
+
+		/// <summary>
+		/// Gets a value indicaing if the plugin has been initialized
+		/// </summary>
+		bool Initialized { get; }
+
+		/// <summary>
+		/// Initializes the plugin
+		/// </summary>
+		void Initialize();
+	}
+}
